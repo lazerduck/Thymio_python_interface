0a4ae98242b4aaba1c72263bd704ce7a54f82847
Adam Bowes <skylinegames@hotmail.co.uk>
2015-01-29 18:51:12 +0000

    amended document after user testing

--- Install.sh
+++ Install.sh
@@ -6,7 +6,7 @@ sudo pip install scratchpy
 echo "installing scratch"
 sudo apt-get install scratch
 echo "installing aseba"    
-sudo add-apt-repository ppa:stephane.magnenat/\'lsb_release -c -s\'
+sudo add-apt-repository ppa:stephane.magnenat/`lsb_release -c -s`
 sudo apt-get update
 sudo apt-get install Aseba
 echo "finished"
